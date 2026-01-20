b1b817c90b6b141e229b110c4e4e32caa0cc38e0
Alice <alice.mazzoleni@psi.ch>
2026-01-20 17:09:39 +0100

    added page not found

--- copy_file_content.py
+++ copy_file_content.py
@@ -5,6 +5,6 @@ with open("binaryfileformat.html", 'r') as f:
     content = f.read()
 
 for file in os.listdir('.'):
-    if file.endswith('.html') and file != "index.html":
+    if file.endswith('.html') and file != "index.html" and file != "page_not_found.html":
         with open(file, 'w') as f:
             f.write(content)
